e8947dd90d68e13d1dbbf8301bd0928faa5cccbe
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-21 08:41:58 +0100

    COnstructor done

---
+++ TDD/Geometry/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace Geometry
+{
+    public class Point
+    {
+        private readonly double _x;
+        private readonly double _y;
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public double X => _x;
+        public double Y => _y;
+    }
+}
---
+++ TDD/GeometryTests/PointTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Geometry;
+using Xunit;
+
+namespace GeometryTests
+{
+    public class PointTests
+    {
+        [Fact]
+        public void Can_create_new_point()
+        {
+            var point = new Point(10, 40);
+
+            point.X.Should().Be(10);
+            point.Y.Should().Be(40);
+        }
+
+        [Fact]
+        public void Can_create_new_point_with_double_parameters()
+        {
+            var point = new Point(1.4, 4.5);
+
+            point.X.Should().Be(1.4);
+            point.Y.Should().Be(4.5);
+        }
+    }
+}
