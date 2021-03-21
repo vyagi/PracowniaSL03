aed051d7e0ac7e65bf1ae6b75675b0c2aacf2ebe
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-21 09:11:21 +0100

    Done

--- TDD/Geometry/Point.cs
+++ TDD/Geometry/Point.cs
@@ -1,9 +1,17 @@
-﻿namespace Geometry
+﻿using System;
+
+namespace Geometry
 {
     public class Point
     {
         private readonly double _x;
         private readonly double _y;
+        public enum ReflectionType
+        {
+            X,
+            Y,
+            Origin
+        }
 
         public Point(double x, double y)
         {
@@ -11,7 +19,28 @@
             _y = y;
         }
 
+        public Point() : this(1,1) { }
+
         public double X => _x;
         public double Y => _y;
+
+        public double Distance() => Math.Sqrt(X*X + Y*Y);
+
+        public double Distance(Point another) => 
+            Math.Sqrt(Math.Pow(X - another.X,2) + Math.Pow(Y - another.Y, 2));
+
+        public static double Distance(Point first, Point second) => 
+            first.Distance(second);
+
+        public override string ToString() => $"({X},{Y})";
+
+        public Point Reflect(ReflectionType reflectionType) =>
+            reflectionType switch
+            {
+                ReflectionType.X => new Point(X, -Y),
+                ReflectionType.Y => new Point(-X, Y),
+                ReflectionType.Origin => new Point(-X, -Y),
+                _ => throw new ArgumentOutOfRangeException(nameof(reflectionType), reflectionType, null)
+            };
     }
 }
--- TDD/GeometryTests/PointTests.cs
+++ TDD/GeometryTests/PointTests.cs
@@ -23,5 +23,89 @@ namespace GeometryTests
             point.X.Should().Be(1.4);
             point.Y.Should().Be(4.5);
         }
+
+        [Fact]
+        public void Can_create_new_point_without_parameters()
+        {
+            var point = new Point();
+
+            point.X.Should().Be(1);
+            point.Y.Should().Be(1);
+        }
+
+        [Fact]
+        public void Distance_is_calculated_correctly()
+        {
+            var point = new Point(3, 4);
+
+            var distance = point.Distance();
+
+            distance.Should().Be(5);
+        }
+
+        [Fact]
+        public void Distance_from_another_point_is_calculated_properly()
+        {
+            var point1 = new Point(-3, 6);
+            var point2 = new Point(3, 14);
+
+            var distance = point1.Distance(point2);
+
+            distance.Should().Be(10);
+        }
+
+        [Fact]
+        public void Static_distance_between_two_points_is_calculated_properly()
+        {
+            var point1 = new Point(-3, 6);
+            var point2 = new Point(3, 14);
+
+            var distance = Point.Distance(point1, point2);
+
+            distance.Should().Be(10);
+        }
+
+        [Fact]
+        public void ToString_returns_valid_string_representation()
+        {
+            var point = new Point(-3, 6);
+
+            var stringRepresentation = point.ToString();
+
+            stringRepresentation.Should().Be("(-3,6)");
+        }
+
+        [Fact]
+        public void Reflection_along_x_should_change_proper_coordinate()
+        {
+            var point = new Point(-3, 6);
+
+            var reflection= point.Reflect(Point.ReflectionType.X);
+
+            reflection.X.Should().Be(-3);
+            reflection.Y.Should().Be(-6);
+        }
+
+        [Fact]
+        public void Reflection_along_y_should_change_proper_coordinate()
+        {
+            var point = new Point(-3, 6);
+
+            var reflection = point.Reflect(Point.ReflectionType.Y);
+
+            reflection.X.Should().Be(3);
+            reflection.Y.Should().Be(6);
+        }
+
+        [Fact]
+        public void Reflection_with_origin_should_change_proper_coordinate()
+        {
+            var point = new Point(-3, 6);
+
+            var reflection = point.Reflect(Point.ReflectionType.Origin);
+
+            reflection.X.Should().Be(3);
+            reflection.Y.Should().Be(-6);
+        }
     }
 }
