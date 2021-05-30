b9ea99a68247c58f418e59dd8bed59555c7305b8
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-05-30 10:16:45 +0200

    Decorator pattern done

---
+++ DesignPatterns/DecoratorPattern/Logger.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NETCore.Encrypt;
+using NETCore.Encrypt.Internal;
+
+namespace DecoratorPattern
+{
+    public interface ILogger
+    {
+        string Log(string message);
+    }
+
+    public class SimpleLogger : ILogger
+    {
+        public string Log(string message) => $"Information: {message}";
+    }
+
+    public class TimeStampingLogger : ILogger
+    {
+        private readonly ILogger _logger;
+
+        public TimeStampingLogger(ILogger logger) => _logger = logger;
+
+        public string Log(string message) => 
+            $"{DateTime.Now:yyyyMMddTHH:mm:ss} {_logger.Log(message)}";
+    }
+
+    public class EncryptingLogger : ILogger
+    {
+        private readonly AESKey _aesKey = EncryptProvider.CreateAesKey();
+
+        private readonly ILogger _logger;
+
+        public EncryptingLogger(ILogger logger) => _logger = logger;
+
+        public string Log(string message) => 
+            EncryptProvider.AESEncrypt(_logger.Log(message), _aesKey.Key);
+    }
+
+
+
+    // public class Logger
+    // {
+    //     public virtual string Log(string message) => $"Information: {message}";
+    // }
+    //
+    // public class TimeStampingLogger : Logger
+    // {
+    //     public override string Log(string message) => 
+    //         $"{DateTime.Now:yyyyMMddTHH:mm:ss} {base.Log(message)}";
+    // }
+    //
+    // public class EncryptingLogger : Logger
+    // {
+    //     private readonly AESKey _aesKey = EncryptProvider.CreateAesKey();
+    //
+    //     public override string Log(string message) => 
+    //         EncryptProvider.AESEncrypt(base.Log(message), _aesKey.Key);
+    // }
+    //
+    // public class EncryptingTimeStampingLogger : Logger
+    // {
+    //     public override string Log(string message)
+    //     {
+    //         return base.Log(message);//TODO
+    //     }
+    // }
+    //
+    // public class TimeStampingEncryptingLogger : Logger
+    // {
+    //     public override string Log(string message)
+    //     {
+    //         return base.Log(message);//TODO
+    //     }
+    // }
+    //
+    // public class CompressingLogger : Logger
+    // {
+    //     public override string Log(string message)
+    //     {
+    //         return base.Log(message);//TODO
+    //     }
+    // }
+}
---
+++ DesignPatterns/DecoratorPatternTests/LoggerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecoratorPattern;
+using FluentAssertions;
+using Xunit;
+
+namespace DecoratorPatternTests
+{
+    public class LoggerTests
+    {
+        [Fact]
+        public void SimpleLogger_log_properly()
+        {
+            var logger = new SimpleLogger();
+            var log = logger.Log("Marcin");
+        
+            log.Should().Be("Information: Marcin");
+        }
+
+        [Fact]
+        public void TimeStampingLogger_log_properly()
+        {
+            var logger = new TimeStampingLogger(new SimpleLogger());
+            var log = logger.Log("Marcin");
+
+            log.Should().StartWith(DateTime.Now.Year.ToString());
+            log.Should().EndWith("Information: Marcin");
+        }
+
+        [Fact]
+        public void EncryptingLogger_log_properly()
+        {
+            var logger = new EncryptingLogger(new SimpleLogger());
+            var log = logger.Log("Marcin");
+        
+            log.Should().NotContain("Marcin");
+        }
+
+        [Fact]
+        public void TimeStampingEncryptingLogger_log_properly()
+        {
+            var logger = new TimeStampingLogger(new EncryptingLogger(new SimpleLogger()));
+            var log = logger.Log("Marcin");
+
+            log.Should().StartWith(DateTime.Now.Year.ToString());
+            log.Should().NotContain("Marcin");
+        }
+
+        [Fact]
+        public void EncryptingTimeStampingLogger_log_properly()
+        {
+            var logger = new EncryptingLogger(new TimeStampingLogger(new SimpleLogger()));
+            var log = logger.Log("Marcin");
+
+            log.Should().NotStartWith(DateTime.Now.Year.ToString());
+            log.Should().NotContain("Marcin");
+        }
+
+        // [Fact]
+        // public void Logger_log_properly()
+        // {
+        //     var logger = new Logger();
+        //     var log = logger.Log("Marcin");
+        //
+        //     log.Should().Be("Information: Marcin");
+        // }
+        //
+        // [Fact]
+        // public void TimeStampingLogger_log_properly()
+        // {
+        //     var logger = new TimeStampingLogger();
+        //     var log = logger.Log("Marcin");
+        //
+        //     log.Should().StartWith(DateTime.Now.Year.ToString());
+        //     log.Should().EndWith("Information: Marcin");
+        // }
+        //
+        // [Fact]
+        // public void EncryptingLogger_log_properly()
+        // {
+        //     var logger = new EncryptingLogger();
+        //     var log = logger.Log("Marcin");
+        //
+        //     log.Should().NotContain("Marcin");
+        // }
+    }
+}
