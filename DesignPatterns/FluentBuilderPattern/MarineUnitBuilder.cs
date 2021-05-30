519e0d2eaeefe710b0cb65999d6d5caa55d6c5e2
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-05-30 08:24:51 +0200

    Marine unit builder stub

---
+++ DesignPatterns/FluentBuilderPattern/MarineUnitBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentBuilderPattern
+{
+    public class MarineUnitBuilder
+    {
+        private readonly MarineUnit _unit = new();
+        
+        private MarineUnitBuilder() { }
+        
+        public static MarineUnitBuilder Initialize() => 
+            new MarineUnitBuilder();
+
+        public object WithName(string name)
+        {
+            _unit.UnitName = name;
+            return this;
+        }
+    }
+}
---
+++ DesignPatterns/MarineUnitConsumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentBuilderPattern;
+
+namespace MarineUnitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newUnit = MarineUnitBuilder
+                .Initialize()
+                .WithName("Cosmos");
+
+            var unit = new MarineUnit();
+        }
+    }
+}
